6f264e57bf0ea7bf2c257b41affc51a080d95423
Aarni Koskela <akx@iki.fi>
2019-11-26 15:35:10 +0200

    Only show info and knobs on default story

--- src/stories/stories.js
+++ src/stories/stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key,react/jsx-curly-newline */
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { boolean, withKnobs } from "@storybook/addon-knobs";
@@ -8,32 +9,35 @@ import "./example-style.css";
 
 const stories = storiesOf("Wheel", module);
 stories.addDecorator(withInfo);
-stories.addParameters({
-  info: {
-    inline: true,
-    text: `Also see the knobs tab below!`
-  }
-});
 stories.addDecorator(withKnobs);
 
-stories.add("with a custom label", () => (
-  <Wheel
-    size={200}
-    snap={boolean("Snap to 10 Degrees", false) ? 10 : undefined}
-    min={boolean("Minimum Zero", false) ? 0 : undefined}
-    max={boolean("Maximum 400", false) ? 400 : undefined}
-    initialDegrees={42}
-    onBeginChange={action("beginChange")}
-    onChangeValue={action("changeValue", { limit: 5 })}
-    onCommitValue={action("commitValue")}
-    renderLabel={({ degrees, style, changing }) => (
-      <div className="my-wheel-label" style={style}>
-        {degrees.toFixed(2)}
-        {changing ? "..." : ""}
-      </div>
-    )}
-  />
-));
+stories.add(
+  "with a custom label",
+  () => (
+    <Wheel
+      size={200}
+      snap={boolean("Snap to 10 Degrees", false) ? 10 : undefined}
+      min={boolean("Minimum Zero", false) ? 0 : undefined}
+      max={boolean("Maximum 400", false) ? 400 : undefined}
+      initialDegrees={42}
+      onBeginChange={action("beginChange")}
+      onChangeValue={action("changeValue", { limit: 5 })}
+      onCommitValue={action("commitValue")}
+      renderLabel={({ degrees, style, changing }) => (
+        <div className="my-wheel-label" style={style}>
+          {degrees.toFixed(2)}
+          {changing ? "..." : ""}
+        </div>
+      )}
+    />
+  ),
+  {
+    info: {
+      inline: true,
+      text: `Also see the knobs tab below!`
+    }
+  }
+);
 
 stories.add("phone number input", () => {
   const [number, setNumber] = React.useState("0000000");
